8884c9fd10e608a41d9f30bde0575d52503648ef
Raphael Kawabata <raphael.kawabata@gmail.com>
2023-01-15 17:05:46 -0300

    Update format.cpp
    update format.cpp

--- src/format.cpp
+++ src/format.cpp
@@ -10,8 +10,13 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 string Format::ElapsedTime(long seconds) {
-    int hour(seconds/HOUR);
-    int minutes((seconds/MINUTE)%MINUTE);
-    int second(seconds%MINUTE);
-    return string(std::to_string(hour) + ":" + std::to_string(minutes) + ":" + std::to_string(second));
-    }
+  int hour(seconds/HOUR);
+  int minutes((seconds/MINUTE)%MINUTE);
+  int second(seconds%MINUTE);
+  string string_minute, string_seconds;
+
+  string_minute = minutes < 10 ? "0" + std::to_string(minutes) : std::to_string(minutes);
+  string_seconds = second < 10 ? "0" + std::to_string(second) : std::to_string(second);
+
+  return string(std::to_string(hour)) + ":" + string_minute + ":" + string_seconds;
+}
